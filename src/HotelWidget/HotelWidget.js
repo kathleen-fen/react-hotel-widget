1ad195c119ff43770d7448a2b302e4d1020e2db9
Kathreen Fenchenko <fev3@mail.ru>
2020-09-26 08:28:52 +0200

    ract-date-range added

--- src/HotelWidget/HotelWidget.js
+++ src/HotelWidget/HotelWidget.js
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
-import JSONP from 'jsonp';
+import React, { useState } from 'react'
+import JSONP from 'jsonp'
+import { DateRange } from 'react-date-range'
+
 import BookingItem from './BookingItem/BookingItem'
 import Loader from './UI/Loader/Loader'
 
-import './HotelWidget.scss';
+import './HotelWidget.scss'
 
 
 
 
 
 function HotelWidget() {
-  const [fromDate, setFromDate] = useState(new Date().toISOString().substring(0, 10))
-  const [toDate, setToDate] = useState(new Date().toISOString().substring(0, 10))
+  const [fromDate, setFromDate] = useState(new Date())
+  const [toDate, setToDate] = useState(new Date())
   const [adultsCount, setAdultsCount] = useState(1)
   const [childrenCount, setChildrenCount] = useState(0)
   const [bookingList, setBookingList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [rangeChoose, setRangeChoose] = useState(false)
 
   const adultsChangeHandler = target => {
     console.log(target)
     setAdultsCount(target.value)
-    
+  }
+
+  const getRange = () => {
+    console.log(fromDate.toUTCString().substring(0,11))
+    return `${fromDate.toUTCString().substring(0,11)}  -  ${toDate.toUTCString().substring(0,11)}`
   }
 
   const listItems = loading ? (<Loader />) : bookingList.map((el, index) => (<BookingItem item={el} key={index}/>))
@@ -29,6 +36,13 @@ function HotelWidget() {
     console.log(target)
     setChildrenCount(target.value)
     
+  }
+
+  const handleSelect = (ranges) => {
+    setFromDate(ranges.selection.startDate)
+    setToDate(ranges.selection.endDate) 
+    console.log(ranges)
+    setRangeChoose(false)
   }
 
   const submitHandler = event => {
@@ -43,20 +57,39 @@ function HotelWidget() {
   console.log("res:",res)
   console.log("err:", err)
   })}
+
+  const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+  }
+  const calendar = rangeChoose ? (<DateRange
+                        className="range-wrapper"
+                        months={2}
+                        minDate={new Date()}
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        showMonthAndYearPickers={false}
+                      />) : null
   
   return (
     <div className="h-container">
       <form onSubmit={submitHandler}>
-        <input type="date" value={fromDate} onChange={(event) => setFromDate(event.target.value)}></input>
-        <input type="date" value={toDate} onChange={(event) => setToDate(event.target.value)}></input>
+      
+        <div onClick={() => setRangeChoose(!rangeChoose)} className="range">
+          <img src="calendar-alt-regular.svg" alt="calendar" />
+          <div className="range__text">{getRange()}</div>
+          {calendar}
+        </div>
         <input type="number" name="adultsCount" value={adultsCount} onChange={event => {adultsChangeHandler(event.target)}}></input>
         <input type="number" name="childrenCount" value={childrenCount} onChange={event => {childrenChangeHandler(event.target)}}></input>
 
         <button onClick={getHotelInfo}>Get</button>
       </form>
+      
       {listItems}
     </div>
-  );
-}
+  )}
 
-export default HotelWidget;
+
+export default HotelWidget
